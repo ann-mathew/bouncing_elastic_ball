7332e7b5ee7b51cbb7ced5c84d9cc43a4c4b50e8
Ann Mathew <ann.mathew@Anns-MBP.bbrouter>
2022-03-08 22:13:47 +0530

    [Richa/Ann] | Refactor. Change ball creation method calls

--- src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
+++ src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
@@ -1,24 +1,27 @@
 package com.tw.dojo.bouncingBall.model;
 
+import static java.util.Arrays.asList;
+
 public class BallFactory {
 
     public static Ball[] all() {
         return new Ball[]{
-                bouncingBall(75, 50, BouncingBall.DOWN),
-                elasticBall(250, 100, Ball.DEFAULT_RADIUS, ElasticBall.SHRINK),
-                bouncingElasticBall(425,150,Ball.DEFAULT_RADIUS,BouncingElasticBall.DOWN,BouncingElasticBall.SHRINK)
+                bouncingBall(75, 50, Ball.DEFAULT_RADIUS, Bounce.DOWN),
+                elasticBall(250, 100, Ball.DEFAULT_RADIUS, Elasticity.SHRINK),
+                bouncingElasticBall(425, 150, Ball.DEFAULT_RADIUS, Bounce.DOWN, Elasticity.SHRINK)
         };
     }
 
-    private static Ball bouncingElasticBall(int centerX, int centerY, int radius, int direction, int growthDirection) {
-        return new BouncingElasticBall(centerX, centerY, radius, direction,growthDirection);
+    public static Ball bouncingElasticBall(int centerX, int centerY, int radius, int direction, int growthDirection) {
+        return new Ball(centerX, centerY, radius, asList(new Bounce(direction),
+                new Elasticity(growthDirection)));
     }
 
-    public static Ball bouncingBall(int centerX, int centerY, int direction) {
-        return new BouncingBall(centerX, centerY, direction);
+    public static Ball bouncingBall(int centerX, int centerY, int radius, int direction) {
+        return new Ball(centerX, centerY, radius, asList(new Bounce(direction)));
     }
 
-    public static Ball elasticBall(int centerX, int centerY, int radius, int direction) {
-        return new ElasticBall(centerX, centerY, radius, direction);
+    public static Ball elasticBall(int centerX, int centerY, int radius, int growthDirection) {
+        return new Ball(centerX, centerY, radius, asList(new Elasticity(growthDirection)));
     }
 }
